64041508b7b8490bea98a191b1d23a54662f8fba
20161104581 <547329654@qq.com>
2018-11-21 12:06:26 +0800

    猜想

--- guess/ViewController.swift
+++ guess/ViewController.swift
@@ -7,14 +7,106 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+     let temprandom:Int = Int(arc4random_uniform(100) + 1)
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func start(_ sender: Any) {
+        let b = Double(result.text!)!
+        if (Int(b) > temprandom)
+        {
+            end.text = "higner "
+        }
+        else if(Int(b) < temprandom)
+        {
+            end.text = "lower"
+        }
+        else {
+            end.text = "correct "
+        }
+        result.text = " "
+        
+        
+    }
+    
+    @IBOutlet weak var end: UILabel!
+    
+    var re = 0
+    @IBAction func number1(_ sender: Any) {
+        if re == 0{
+            
+            result.text = result.text! + "1"
+        }
+    }
+    @IBAction func number2(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "2"
+        }
+    }
+    
+    @IBAction func number3(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "3"
+        }
+    }
+    
+    @IBAction func number4(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "4"
+        }
+    }
+    
+    @IBAction func number5(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "5"
+        }
+    }
+    
+    @IBAction func number6(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "6"
+        }
+    }
+    
+    @IBAction func number7(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "7"
+        }
+    }
+    
+    @IBAction func number8(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "8"
+        }
+    }
+    
+    @IBAction func number9(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "9"
+        }
+    }
+    
+    @IBAction func number0(_ sender: Any) {
+        if re == 0{
+            result.text = result.text! + "0"
+        }
+    }
+    
+    
+    
+    
+      //let answer = randomIntBetween(1,100)
+    
+    
+    
+    
+    
 }
+    
+
 
